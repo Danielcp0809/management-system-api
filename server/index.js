145e13298c3ee2646207a03f7278af4856595f97
Daniel Cordova <jdany0809@hotmail.com>
2022-08-20 01:14:49 -0700

    GraphQL root query and client schema added

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-require('dotenv').config();
-const schema = require('./schema/schema')
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+require("dotenv").config();
+const schema = require("./schema/schema");
 const port = process.env.PORT || 5000;
 const app = express();
 
-
-app.use('/graphql', graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema,
-    graphiql: process.env.NODE_ENV === 'development',
-
-}))
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
 
 app.listen(port, console.log(`Server running on port ${port}`));
-
